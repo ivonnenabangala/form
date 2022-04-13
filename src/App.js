05117258d80357b901092d51224ba1d09972372a
Ivonne Nabangala <ivonne@debian-BULLSEYE-live-builder-AMD64>
2022-04-13 17:16:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const formReducer = (state, event) => {
+  if(event.reset) {
+    return {
+      firstname: '',
+      lastname: '',
+      email: '',
+      gender: '',
+      address: '',
+      bio: '',
+    }
+  }
+ return {
+   ...state,
+   [event.name]: event.value
+ }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+    console.log(event.target.elements.firstname.value)
+    console.log(event.target.elements.lastname.value)
+    console.log(event.target.elements.email.value)
+    console.log(event.target.elements.gender.value)
+    console.log(event.target.elements.address.value)
+    console.log(event.target.elements.bio.value)
+
+
+    setTimeout(() => {
+      setSubmitting(false);
+      setFormData({
+        reset: true
+      })
+    }, 4000);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  return(
+    <div className="wrapper">
+    <h1>Fill the form below</h1>
+    {submitting &&
+     <div>
+       You are submitting the following:
+       <ul>
+         {Object.entries(formData).map(([name, value]) => (
+           <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+         ))}
+       </ul>
+     </div>
+    }
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <label>
+          <p>First Name</p>
+          <input placeholder='First Name' name="firstname" onChange={handleChange} value={formData.firstname || ''}/>
+        </label>
+        <label>
+          <p>Last Name</p>
+          <input placeholder='Last Name' name="lastname" onChange={handleChange} value={formData.lastname || ''}/>
+        </label>
+        <label>
+          <p>Email</p>
+          <input placeholder='example@gmail.com' name="email" onChange={handleChange} value={formData.email || ''}/>
+        </label>
+        <label>
+           <p>Gender</p>
+           <select name="gender" onChange={handleChange} value={formData.gender || ''}>
+               <option value="">Please select your gender</option>
+               <option value="male">Female</option>
+               <option value="male">Male</option>
+               <option value="transgender">Transgender</option>
+               <option value="null">Prefer not to say</option>
+           </select>
+         </label>
+         <label>
+          <p>Address</p>
+          <input placeholder="Postal address" name="address" onChange={handleChange} value={formData.address || ''}/>
+        </label>
+        <label>
+          <p>Bio</p>
+          <textarea placeholder="300 words" rows="4" cols="50" name="bio" onChange={handleChange} value={formData.bio || ''}/>
+        </label>
+      </fieldset>
+      <button style={{width: "64px"}} type="submit">Submit</button>
+    </form>
+  </div>
+  )
 }
 
 export default App;
